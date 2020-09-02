9bfc0178c0ba1d8543a05e2c8e8c83099f164c33
CincoSwim <cgpucko@gmail.com>
2020-09-01 21:04:26 -0400

    Added Comments

--- hw0c.cpp
+++ hw0c.cpp
@@ -1,9 +1,14 @@
+//Name: Christopher Pucko
+//Course: EECS 2510 - Non-Linear Data Structures
+//Assignment - Homework 0, Part C
+
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
 int countDownRuns(int* arrayPtr, int arrayCt){
+    //Counts Number of Down Runs in the array passed.
     bool isGreaterOrEqual = false;
     int runCt = 0;
     int i = 0;
@@ -20,7 +25,7 @@ int countDownRuns(int* arrayPtr, int arrayCt){
         }
         else if (arrayPtr[i]< arrayPtr[i+1]){
             i++;
-            isGreaterOrEqual = false;
+            isGreaterOrEqual = false; //Up run hit, so toggle flag & check next
         }
 
     }
@@ -29,6 +34,7 @@ int countDownRuns(int* arrayPtr, int arrayCt){
     return runCt;
 }
 int countUpRuns(int* arrayPtr, int arrayCt){
+    //Counts number of Up runs in the array passed.
     bool isGreaterOrEqual = false;
     int runCt = 0;
     int i = 0;
@@ -45,7 +51,7 @@ int countUpRuns(int* arrayPtr, int arrayCt){
         }
         else if (arrayPtr[i]> arrayPtr[i+1]){
             i++;
-            isGreaterOrEqual = false;
+            isGreaterOrEqual = false; //Down Run hit, so toggle flag & check next.
         }
 
     }
@@ -54,6 +60,7 @@ int countUpRuns(int* arrayPtr, int arrayCt){
     return runCt;
 }
 int* returnsArrayPointer(){
+    //Reads values from .txt file to array, then returns pointer location of array
     static int readArray[100], rdNum;
     string line;
     ifstream numFile;
@@ -80,15 +87,21 @@ int main() {
     arrayPoint = returnsArrayPointer();
     arrayCt = 0;
 
+    //Counts elements in array to find "end" of practical array.
+    //
+    //This accounts for the number of 0 values appended to the end of the large array, preventing miscount
+    //of runs.
+    //
+    //Assumes end of array is at index before a run of three 0 values.
     for (int i = 0; i <= 100; i++ ){
-        if (arrayPoint[i] == 0 && arrayPoint[i+1]==0 && arrayPoint[i+2]==0) {
+        if (arrayPoint[i] == 0 && arrayPoint[i+1]==0 && arrayPoint[i+2]==0) {    //ArrayList<> will be missed...
             arrayCt = i - 1;
             break;
         }
     }
 
 
-    cout << "Elements loaded: " << arrayCt <<endl;
+    cout << "Numbers loaded: " << arrayCt <<endl;
     upRunCt = countUpRuns(arrayPoint, arrayCt);
     dwnRunCt = countDownRuns(arrayPoint, arrayCt);
     cout << "Up Runs Found: " << upRunCt <<endl;
